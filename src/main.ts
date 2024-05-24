48ecd8cb8a5674ba688e00e5f9364e0362328176
Md.Rafiul Islam Shohan <ris.shohan@gmail.com>
2024-05-24 20:43:59 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule,{cors:false});
 
 /**
    * Validation Pipe for formating validation error
