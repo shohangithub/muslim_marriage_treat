55dac7e3ad66baea323b3249cbae4b80ef06e624
Rafiul Islam <sohan@redlimesolutions.com>
2024-05-31 11:16:03 +0600

    use auth guard in endpoint

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -9,11 +9,13 @@ import {
   Ip,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { CompleteBookingDto, UpdateBookingDto } from './dto/update-booking.dto';
 import { BookingQueryDto } from './dto/booking-query.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('booking')
 export class BookingController {
@@ -24,12 +26,12 @@ export class BookingController {
     createBookingDto.bookedFrom = ip;
     return this.bookingService.create(createBookingDto);
   }
-
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.bookingService.findAll();
   }
-
+  @UseGuards(AuthGuard)
   @Get('pagination')
   pagination(@Query() paginationQuery: BookingQueryDto) {
     return this.bookingService.pagination(paginationQuery);
@@ -40,6 +42,7 @@ export class BookingController {
     return this.bookingService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBookingDto: UpdateBookingDto) {
     return this.bookingService.update(+id, updateBookingDto);
@@ -52,21 +55,25 @@ export class BookingController {
   ) {
     return this.bookingService.completeBooking(+id, updateBookingDto);
   }
-
+  @UseGuards(AuthGuard)
   @Put('cancel-booking/:id')
   cancelBooking(@Param('id') id: number) {
     return this.bookingService.cancelBooking(id);
   }
+
+  @UseGuards(AuthGuard)
   @Put('confirm-booking/:id')
   confirmBooking(@Param('id') id: number) {
     return this.bookingService.confirmBooking(id);
   }
 
+  @UseGuards(AuthGuard)
   @Put('refund-booking/:id')
   refundBooking(@Param('id') id: number) {
     return this.bookingService.refundBooking(id);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.bookingService.remove(+id);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository:Repository<User>
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.userRepository.save(createUserDto);
+  async create(createUserDto: CreateUserDto) {
+    if (await this.userRepository.existsBy({ email: createUserDto.email }))
+      throw new HttpException(
+        `This email already exists !`,
+        HttpStatus.BAD_REQUEST,
+      );
+
+    const password = createUserDto.password;
+    createUserDto.password = await bcrypt.hash(password, 10);
+    const response = await this.userRepository.save(createUserDto);
+    response.password = password;
+    return response;
   }
 
   findAll() {
@@ -21,14 +32,14 @@ export class UserService {
   }
 
   findOne(id: number) {
-    return this.userRepository.findOneBy({id})
+    return this.userRepository.findOneBy({ id });
   }
   findByEmail(email: string) {
-    return this.userRepository.findOneBy({email:email})
+    return this.userRepository.findOneBy({ email: email });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return this.userRepository.update(id,updateUserDto);
+    return this.userRepository.update(id, updateUserDto);
   }
 
   remove(id: number) {
